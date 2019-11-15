ea9e4af2865ae5865f9396a5a62693c559eb82d5
RoZvEr <httodoroff@gmail.com>
2019-11-16 00:30:15 +0200

    Edit image preprocessing script - fix the np array bug

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -8,17 +8,18 @@ counter = 0
 images_with_file_names = []
 
 # Small batch of the dataset we will be using to try to construct the neural network, not to train with.
-BATCH_SIZE = 5000
+BATCH_SIZE = 4999
 
 for curr_img in images:
     path = os.path.join(IMAGES_DIR, curr_img)
     img = cv2.imread(path)
-    images_with_file_names.append([img, curr_img])
+    img = cv2.resize(img, (128, 128))
+    images_with_file_names.append(img)
     if counter == BATCH_SIZE:
         break
     counter = counter + 1
 
-images_with_file_names = np.array(images_with_file_names)
+images_with_file_names = np.array(images_with_file_names).reshape(-1, 128, 128, 3)
 
 with open('dataset.npy', 'wb') as output_file:
     np.save(output_file, images_with_file_names)
