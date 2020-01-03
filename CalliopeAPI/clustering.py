8f1095517741d4affaaa54b3a5df2b1db02dddb7
RoZvEr <httodoroff@gmail.com>
2020-01-03 19:59:40 +0200

    Added cluster labels serialization and load_dataset.py edited

--- CalliopeAPI/clustering.py
+++ CalliopeAPI/clustering.py
@@ -5,25 +5,21 @@ from tensorflow.keras.applications.vgg16 import VGG16
 
 
 # Extract features from each image and add them to a list
-def get_feature_list(model, image_dataset, batch_size):
-    counter = 0
+def get_feature_list(model, image_dataset):
     feature_list = []
     for img in image_dataset:
         current_feature = model.predict(np.expand_dims(img, axis=0))
         current_feature = np.array(current_feature)
         feature_list.append(current_feature.flatten())
-        if counter == batch_size-1:
-            break
-        counter = counter+1
     return feature_list
 
 
 # Display the images with their corresponding cluster
-def show_images(image_dataset, clustered_areas, batch_size):
+def show_images(image_dataset, cluster_labels, batch_size=100):
     idx = 0
     for i in image_dataset:
         plt.imshow(i)
-        plt.xlabel(clustered_areas[idx])
+        plt.xlabel(cluster_labels[idx])
         plt.show()
         if idx == batch_size-1:
             break
@@ -31,27 +27,39 @@ def show_images(image_dataset, clustered_areas, batch_size):
 
 
 def main():
-
-    # Number of images to be clustered and number of different clusters
-    batch_size = 100
-    num_clusters = 16
+    # Number of images to be shown after clustering, number of different clusters and clustered labels location
+    num_clusters = 32
+    clustered_labels_location = 'dataset/cluster_labels.npy'
 
     # Pre-trained model - VGG16
+    print('Loading VGG16...')
     model = VGG16(weights='imagenet', include_top=False)
-    print(model.summary())
+    print('Finished')
 
     # Load dataset - only images, without labels
-    image_dataset = np.load('dataset/dataset.npy', allow_pickle=True)
+    print('Loading dataset...')
+    image_dataset = np.load('dataset/images_dataset.npy', allow_pickle=True)
+    print('Finished')
 
-    # Cluster the images based on features
-    feature_list = get_feature_list(model, image_dataset, batch_size)
-    clustered = KMeans(n_clusters=num_clusters, random_state=0).fit(feature_list)
+    # Extract features from each image and add them to a list
+    print('Getting feature list...')
+    feature_list = get_feature_list(model, image_dataset)
+    print('Finished')
 
-    # Get where each image was clustered
-    clustered_areas = clustered.labels_
-    print(clustered_areas)
+    # Obtain the corresponding centroid index for each image in the dataset
+    # Note: 42 is the answer to everything
+    clustered = KMeans(n_clusters=num_clusters, random_state=42).fit(feature_list)
 
-    show_images(image_dataset, clustered_areas, batch_size)
+    # Transform the labels into numpy array
+    cluster_labels = np.array(clustered.labels_)
+
+    # Serialize the clustered labels
+    print('Serializing labels....')
+    np.save(clustered_labels_location, cluster_labels)
+    print('Finished')
+
+    # Optional: show batch_size image with their corresponding cluster
+    # show_images(image_dataset, cluster_labels, batch_size=100)
 
 
 if __name__ == '__main__':
