1b6a9a7081eaea55931a5737fc7fae65bd4bc09b
RoZvEr <httodoroff@gmail.com>
2020-04-07 22:04:44 +0300

    Add secret settings

--- web-app/Calliope/settings.py
+++ web-app/Calliope/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .secret_settings import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v+p#$=&qaj*(43l8(z_3686#5jy7c@2&vg_gw0t$w%&b4@_&x#'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
